e9cfb60c61533f919c8eb846ad876c1a9de2d977
Komorebi <25750337479@qq.com>
2022-11-22 21:35:02 +0800

    添加查询customer功能

--- shilee-manager/src/main/java/com/example/shileemanager/mapper/MainMapper.java
+++ shilee-manager/src/main/java/com/example/shileemanager/mapper/MainMapper.java
@@ -1,8 +1,11 @@
 package com.example.shileemanager.mapper;
 
+import com.example.shileemanager.entity.Customer;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MainMapper {
-
+    public List<Customer> getCustomers();
 }
