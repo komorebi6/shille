064785df30c387c5b0870520e4a8d73f0435fa52
Komorebi <25750337479@qq.com>
2022-11-22 19:21:45 +0800

    初始版本

---
+++ shilee-manager/src/main/java/com/example/shileemanager/ShileeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shileemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShileeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShileeManagerApplication.class, args);
+    }
+
+}
---
+++ shilee-manager/src/test/java/com/example/shileemanager/ShileeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shileemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShileeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
